337b735c073cdaf87ec64da2eaa14ca5afad17fe
asicuima <liangma422@gmail.com>
2025-05-11 21:11:30 +0200

    Update repository name to myspace

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { useLocation } from "react-router-dom";
@@ -20,48 +20,50 @@ function App() {
   }, [location]);
 
   return (
-    <div className="App">
-      <HelmetProvider>
-        <Helmet>
-          <title>asicuima</title>
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://asicuima.com/" />
-          <meta property="og:title" content="asicuima" />
-          <meta property="og:description" content="A website all about Liangliang Ma." />
-          <meta property="og:image" content={profileImage} />
+    <HashRouter>
+      <div className="App">
+        <HelmetProvider>
+          <Helmet>
+            <title>asicuima</title>
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://asicuima.github.io/myspace/" />
+            <meta property="og:title" content="asicuima" />
+            <meta property="og:description" content="A website all about Liangliang Ma." />
+            <meta property="og:image" content={profileImage} />
 
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:url" content="https://asicuima.com/" />
-          <meta name="twitter:title" content="asicuima" />
-          <meta name="twitter:description" content="A website all about Liangliang Ma." />
-          <meta name="twitter:image" content={profileImage} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:url" content="https://asicuima.github.io/myspace/" />
+            <meta name="twitter:title" content="asicuima" />
+            <meta name="twitter:description" content="A website all about Liangliang Ma." />
+            <meta name="twitter:image" content={profileImage} />
 
-          <meta name="title" content="asicuima" />
-          <meta name="description" content="A website all about Liangliang Ma." />
-          <meta name="author" content="Liangliang Ma" />
-        </Helmet>
-        <Container className="pt-5">
-          <Navbar expand="lg">
-            <Navbar.Brand href="/">Liangliang Ma</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                <Nav.Link href="/">About</Nav.Link>
-                <Nav.Link href={cv} target="_blank" rel="noreferrer">CV</Nav.Link>
-                <Nav.Link href="/elsewhere">Elsewhere</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <hr className='mb-5' />
+            <meta name="title" content="asicuima" />
+            <meta name="description" content="A website all about Liangliang Ma." />
+            <meta name="author" content="Liangliang Ma" />
+          </Helmet>
+          <Container className="pt-5">
+            <Navbar expand="lg">
+              <Navbar.Brand href="/">Liangliang Ma</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                  <Nav.Link href="/">About</Nav.Link>
+                  <Nav.Link href={cv} target="_blank" rel="noreferrer">CV</Nav.Link>
+                  <Nav.Link href="/#/elsewhere">Elsewhere</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <hr className='mb-5' />
 
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/cv" element={<CV />} />
-            <Route path="/elsewhere" element={<Elsewhere />} />
-          </Routes>
-        </Container>
-      </HelmetProvider>
-    </div>
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="/cv" element={<CV />} />
+              <Route path="/elsewhere" element={<Elsewhere />} />
+            </Routes>
+          </Container>
+        </HelmetProvider>
+      </div>
+    </HashRouter>
   );
 }
 
